a53e677be684c96c8ad83b61b34a3fc8df1d94a0
jvitorfranca <vitoorbr@gmail.com>
2018-04-25 01:12:50 -0300

    updating .h

--- sllist/sllist.h
+++ sllist/sllist.h
@@ -18,14 +18,18 @@ typedef struct _sll_{
     SLList *sllCreate();
     int sllDestroy(SLList *l);
     int sllInsertFirst(SLList *l, void *data);
+    int sllInsertLast(SLList *list, void *data)
     void *sllRemoveFirst(SLList *l);
+    int sllNumNodes(SLList *list);
 
   #else
 
     extern SLList *sllCreate();
     extern int sllDestroy(SLList *l);
     extern int sllInsertFirst(SLList *l, void *data);
+    extern int sllInsertLast(SLList *list, void *data);
     extern void *sllRemoveFirst(SLList *l);
+    extern int sllNumNodes(SLList *list);
 
   #endif
 #endif
